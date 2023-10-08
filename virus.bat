f593ff44058c5a83ae440fa07bb8c85322bd7543
calipti <70285643+calipti@users.noreply.github.com>
2023-10-08 16:22:10 -0700

    Add files via upload

---
+++ virus.bat
@@ -0,0 +1,100 @@
+start iexplore.exe https://www.dentalimplantpro.com/wp-content/uploads/sites/146/2017/08/Arnold-schwarzenegger.jpg
+@echo
+start iexplore.exe https://www.dentalimplantpro.com/wp-content/uploads/sites/146/2017/08/Arnold-schwarzenegger.jpg
+@echo
+start iexplore.exe https://www.dentalimplantpro.com/wp-content/uploads/sites/146/2017/08/Arnold-schwarzenegger.jpg
+start iexplore.exe https://www.dentalimplantpro.com/wp-content/uploads/sites/146/2017/08/Arnold-schwarzenegger.jpg
+@echo
+start iexplore.exe https://www.dentalimplantpro.com/wp-content/uploads/sites/146/2017/08/Arnold-schwarzenegger.jpg
+@echo
+start iexplore.exe https://www.dentalimplantpro.com/wp-content/uploads/sites/146/2017/08/Arnold-schwarzenegger.jpg
+@echo
+start iexplore.exe https://www.dentalimplantpro.com/wp-content/uploads/sites/146/2017/08/Arnold-schwarzenegger.jpg
+@echo
+start iexplore.exe https://www.dentalimplantpro.com/wp-content/uploads/sites/146/2017/08/Arnold-schwarzenegger.jpg
+@echo
+start iexplore.exe https://www.dentalimplantpro.com/wp-content/uploads/sites/146/2017/08/Arnold-schwarzenegger.jpg
+@echo
+start iexplore.exe https://www.dentalimplantpro.com/wp-content/uploads/sites/146/2017/08/Arnold-schwarzenegger.jpg
+@echo
+start iexplore.exe https://www.dentalimplantpro.com/wp-content/uploads/sites/146/2017/08/Arnold-schwarzenegger.jpg
+@echo
+start iexplore.exe https://www.dentalimplantpro.com/wp-content/uploads/sites/146/2017/08/Arnold-schwarzenegger.jpg
+@echo
+start iexplore.exe https://www.dentalimplantpro.com/wp-content/uploads/sites/146/2017/08/Arnold-schwarzenegger.jpg
+@echo
+start iexplore.exe https://www.dentalimplantpro.com/wp-content/uploads/sites/146/2017/08/Arnold-schwarzenegger.jpg
+@echo
+start iexplore.exe https://www.dentalimplantpro.com/wp-content/uploads/sites/146/2017/08/Arnold-schwarzenegger.jpg
+@echo
+start iexplore.exe https://www.dentalimplantpro.com/wp-content/uploads/sites/146/2017/08/Arnold-schwarzenegger.jpg
+@echo
+start iexplore.exe https://www.dentalimplantpro.com/wp-content/uploads/sites/146/2017/08/Arnold-schwarzenegger.jpg
+@echo
+start iexplore.exe https://www.dentalimplantpro.com/wp-content/uploads/sites/146/2017/08/Arnold-schwarzenegger.jpg
+@echo
+start iexplore.exe https://www.dentalimplantpro.com/wp-content/uploads/sites/146/2017/08/Arnold-schwarzenegger.jpg
+@echo
+start iexplore.exe https://www.dentalimplantpro.com/wp-content/uploads/sites/146/2017/08/Arnold-schwarzenegger.jpg
+@echo
+start iexplore.exe https://www.dentalimplantpro.com/wp-content/uploads/sites/146/2017/08/Arnold-schwarzenegger.jpg
+@echo
+start iexplore.exe https://www.dentalimplantpro.com/wp-content/uploads/sites/146/2017/08/Arnold-schwarzenegger.jpg
+@echo
+start iexplore.exe https://www.dentalimplantpro.com/wp-content/uploads/sites/146/2017/08/Arnold-schwarzenegger.jpg
+@echo
+start iexplore.exe https://www.dentalimplantpro.com/wp-content/uploads/sites/146/2017/08/Arnold-schwarzenegger.jpg
+@echo
+start iexplore.exe https://www.dentalimplantpro.com/wp-content/uploads/sites/146/2017/08/Arnold-schwarzenegger.jpg
+@echo
+start iexplore.exe https://www.dentalimplantpro.com/wp-content/uploads/sites/146/2017/08/Arnold-schwarzenegger.jpg
+@echo
+start iexplore.exe https://www.dentalimplantpro.com/wp-content/uploads/sites/146/2017/08/Arnold-schwarzenegger.jpg
+@echo
+start iexplore.exe https://www.dentalimplantpro.com/wp-content/uploads/sites/146/2017/08/Arnold-schwarzenegger.jpg
+@echo
+start iexplore.exe https://www.dentalimplantpro.com/wp-content/uploads/sites/146/2017/08/Arnold-schwarzenegger.jpg
+@echo
+start iexplore.exe https://www.dentalimplantpro.com/wp-content/uploads/sites/146/2017/08/Arnold-schwarzenegger.jpg
+@echo
+start iexplore.exe https://www.dentalimplantpro.com/wp-content/uploads/sites/146/2017/08/Arnold-schwarzenegger.jpg
+@echo
+start iexplore.exe https://www.dentalimplantpro.com/wp-content/uploads/sites/146/2017/08/Arnold-schwarzenegger.jpg
+@echo
+start iexplore.exe https://www.dentalimplantpro.com/wp-content/uploads/sites/146/2017/08/Arnold-schwarzenegger.jpg
+@echo
+start iexplore.exe https://www.dentalimplantpro.com/wp-content/uploads/sites/146/2017/08/Arnold-schwarzenegger.jpg
+@echo
+start iexplore.exe https://www.dentalimplantpro.com/wp-content/uploads/sites/146/2017/08/Arnold-schwarzenegger.jpg
+@echo
+start iexplore.exe https://www.dentalimplantpro.com/wp-content/uploads/sites/146/2017/08/Arnold-schwarzenegger.jpg
+@echo
+start iexplore.exe https://www.dentalimplantpro.com/wp-content/uploads/sites/146/2017/08/Arnold-schwarzenegger.jpg
+@echo
+start iexplore.exe https://www.dentalimplantpro.com/wp-content/uploads/sites/146/2017/08/Arnold-schwarzenegger.jpg
+@echo
+start iexplore.exe https://www.dentalimplantpro.com/wp-content/uploads/sites/146/2017/08/Arnold-schwarzenegger.jpg
+@echo
+start iexplore.exe https://www.dentalimplantpro.com/wp-content/uploads/sites/146/2017/08/Arnold-schwarzenegger.jpg
+@echo
+start iexplore.exe https://www.dentalimplantpro.com/wp-content/uploads/sites/146/2017/08/Arnold-schwarzenegger.jpg
+@echo
+start iexplore.exe https://www.dentalimplantpro.com/wp-content/uploads/sites/146/2017/08/Arnold-schwarzenegger.jpg
+@echo
+start iexplore.exe https://www.dentalimplantpro.com/wp-content/uploads/sites/146/2017/08/Arnold-schwarzenegger.jpg
+@echo
+start iexplore.exe https://www.dentalimplantpro.com/wp-content/uploads/sites/146/2017/08/Arnold-schwarzenegger.jpg
+@echo
+start iexplore.exe https://www.dentalimplantpro.com/wp-content/uploads/sites/146/2017/08/Arnold-schwarzenegger.jpg
+@echo
+start iexplore.exe https://www.dentalimplantpro.com/wp-content/uploads/sites/146/2017/08/Arnold-schwarzenegger.jpg
+@echo
+start iexplore.exe https://www.dentalimplantpro.com/wp-content/uploads/sites/146/2017/08/Arnold-schwarzenegger.jpg
+@echo
+start iexplore.exe https://www.dentalimplantpro.com/wp-content/uploads/sites/146/2017/08/Arnold-schwarzenegger.jpg
+@echo
+start iexplore.exe https://www.dentalimplantpro.com/wp-content/uploads/sites/146/2017/08/Arnold-schwarzenegger.jpg
+@echo
+
+
+
